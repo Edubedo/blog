af5818176b9ee21615dcb1eb30df1ab8ee22dab8
eescobedo <ezescobedo27@gmail.com>
2025-03-31 09:51:17 -0600

    starter blog git#

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -26,14 +26,14 @@ export default function BlogPosts() {
           .map((post) => (
             <Link
               key={post.slug}
-              className="flex flex-col space-y-1 mb-5 transition-opacity duration-200 hover:opacity-80"
+              className="flex flex-col mb-5 space-y-1 transition-opacity duration-200 hover:opacity-80"
               href={`/blog/${post.slug}`}
             >
-              <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
+              <div className="flex flex-col items-start justify-between w-full space-y-1 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-2">
                 <h2 className="text-black dark:text-white">
                   {post.metadata.title}
                 </h2>
-                <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
+                <p className="text-sm text-neutral-600 dark:text-neutral-400 tabular-nums">
                   {formatDate(post.metadata.publishedAt, false)}
                 </p>
               </div>
